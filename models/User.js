0a8be7c7466e051e70a44e39b2019498a8596c7d
AxelCEntretien <axel.charrier@icloud.com>
2025-06-02 18:21:38 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,11 @@ const userSchema = new mongoose.Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    street: String,     
+    number: String,    
+    zipcode: String,  
+    city: String 
 });
 
 const User = mongoose.model('User', userSchema);
